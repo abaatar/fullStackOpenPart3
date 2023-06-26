87e12cdb8846eb1546b0fcc4306507b7218e4e27
Altangerel Baatar <abaatar@Altangerels-MacBook-Pro.local>
2023-06-26 19:54:46 +0800

    Exercises 3.15-3.18 are done!

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ require("dotenv").config();
 
 const Person = require("./models/person");
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
@@ -18,55 +26,11 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
     res.json(persons);
   });
 });
-
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
-});
-
-app.get("/info", (req, res) => {
-  res.send(`Phonebook has info for ${persons.length} people<br/>${new Date()}`);
-});
-
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  res.status(204).end();
-});
-
-const generateId = () => {
-  return Math.floor(Math.random() * 100000);
-};
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
@@ -81,13 +45,7 @@ app.post("/api/persons", (req, res) => {
       error: "number is missing",
     });
   }
-  /*
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
-  }
-*/
+
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -97,6 +55,47 @@ app.post("/api/persons", (req, res) => {
     res.json(savedPerson);
   });
 });
+
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.get("/info", (req, res) => {
+  Person.find({}).then((persons) => {
+    res.send(
+      `Phonebook has info for ${persons.length} people<br/>${new Date()}`
+    );
+  });
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
